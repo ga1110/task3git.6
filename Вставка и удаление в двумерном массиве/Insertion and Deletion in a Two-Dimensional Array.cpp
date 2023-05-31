b7f9ba22329aad4cf4a28131b29b81a739ac5f6e
ga1110 <destin321@mail.ru>
2023-05-31 15:20:25 +0400

    Update Insertion and Deletion in a Two-Dimensional Array.cpp
    Удалить все строки, содержащие минимальные элементы

--- Вставка и удаление в двумерном массиве/Insertion and Deletion in a Two-Dimensional Array.cpp
+++ Вставка и удаление в двумерном массиве/Insertion and Deletion in a Two-Dimensional Array.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
-int** create(int n, int h) { // функция создания и ввода двумерного массива 
+int** create(int n, int h) {// функция создания и ввода двумерного массива 
     int** m = new int* [2 * n + 1];
     for (int i = 0; i < 2 * n + 1; i++) {
         m[i] = new int[2 * n + 1];
@@ -14,24 +14,18 @@ int** create(int n, int h) { // функция создания и ввода д
     cout << "\n";
     return m;
 }
-int* create_x(int n) { // функция создания и вывода столбца X
-    int* m = new int[n];
+int min(int** m, int n, int h) {// функия поиска минимального элемента 
+    int x = m[0][0];
     for (int i = 0; i < n; i++) {
-        cout << "x [" << i << "] = "; cin >> m[i];
-    }
-    cout << "\n";
-    cout << "X = (";
-    for (int i = 0; i < n; i++) {
-        if (i != n - 1) {
-            cout << m[i] << " ";
-        }
-        else {
-            cout << m[i];
+        for (int j = 0; j < h; j++) {
+            if (x > m[i][j]) {
+                x = m[i][j];
+            }
         }
     }
-    cout << ")";
+    cout << "Min = " << x;
     cout << "\n";
-    return m;
+    return x;
 }
 int output(int** m, int n, int h) {// функция вывода
     for (int i = 0; i < n; i++) {
@@ -43,28 +37,26 @@ int output(int** m, int n, int h) {// функция вывода
     cout << "\n";
     return 0;
 }
-int check(int** m, int j, int n) {// функция проверки столбца
-    for (int i = 0; i < n; i++) {
-        if (m[i][j] % 2 == 0) {
+int check(int** m, int i, int h, int x) {// функция проверки строки
+    for (int j = 0; j < h; j++) {
+        if (m[i][j] == x) {
             return 1;
         }
     }
     return 0;
 }
-int** res(int** m, int* x, int n, int& h) {// функция решения
-    for (int j = 0; j < h; j++) {
-        if (check(m, j, n) == 0) { // если все элементы нечетные 
-            h++;
-            for (int y = 0; y < n; y++) {
-                for (int u = h; u > j; u--) { //сдвигаем элементы вправо
-                    m[y][u] = m[y][u - 1];
+int** res(int** m, int x, int& n, int h) {// функция решения 
+    for (int i = 0; i < n; i++) {
+        if (check(m, i, h, x) == 1) { // если строка содержит минимальный элемент
+            for (int j = i; j < n; j++) {
+                for (int t = 0; t < h; t++) {// сдвигаем строку i вниз
+                    m[j][t] = m[j + 1][t];
                 }
-                m[y][j] = x[y];
 
             }
-            j++;
+            n--;
+            i--;
         }
-
     }
     return m;
 }
@@ -74,8 +66,8 @@ int main()
     int n, h;
     cout << "n = "; cin >> n;
     cout << "h = "; cin >> h;
-    int* x = create_x(n);
     int** m = create(n, h);
+    int x = min(m, n, h);
     output(m, n, h);
     m = res(m, x, n, h);
     output(m, n, h);
