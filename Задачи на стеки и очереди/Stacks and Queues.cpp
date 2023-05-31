4f3cd309f9d6e027533ba4bff746c34006f1833d
ga1110 <destin321@mail.ru>
2023-05-31 15:30:51 +0400

    Create Stacks and Queues.cpp
    Создать стек, содержащий целые числа. Удалить все простые числа. Порядок следования в результате должен совпадать с порядком следования ввода. Например, было 1 2 3 4 5 6 7 8 9. Стало 1 4 6 8 9

---
+++ Задачи на стеки и очереди/Stacks and Queues.cpp
@@ -0,0 +1,77 @@
+﻿
+#include <iostream>
+using namespace std;
+
+bool prime_num(int x) {
+    if (x == 0 || x == 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(x); i++) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+struct stack {
+    int inf;
+    stack* next;
+};
+
+void push(stack*& h, int x) {
+    stack* r = new stack;
+    r->inf = x;
+    r->next = h;
+    h = r;
+}
+
+int pop(stack*& h) {
+    int i = h->inf;
+    stack* r = h;
+    h = h->next;
+    delete r;
+    return i;
+}
+
+void reverse(stack*& h) {
+    stack* head1 = NULL;
+    while (h) {
+        push(head1, pop(h));
+    }
+    h = head1;
+}
+
+void print(stack*& h) {
+    while (h) {
+        cout << pop(h) << " ";
+        
+    }
+    cout << endl;
+}
+
+stack* result(stack*& h) {
+    stack* res = NULL;
+    stack* h1 = NULL;
+    while (h) {
+        int x = pop(h);
+        if (prime_num(x) == false) {
+            push(res, x);
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cout << "n = "; cin >> n;
+    stack* head = NULL;
+    int x;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        push(head, x);
+    }
+    stack* res = result(head);
+    print(res);
+}
+
