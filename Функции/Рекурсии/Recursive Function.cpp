97fa811ac0dc9e021b4bded7c2dbc11225839c0b
ga1110 <destin321@mail.ru>
2023-05-31 15:25:23 +0400

    Create Recursive Function.cpp

---
+++ Функции/Рекурсии/Recursive Function.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+long double factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    if (n < 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return n*factorial(n - 1);
+    }
+}
+int main()
+{
+    int n, f;
+    cout << "n = "; cin >> n;
+    f = 1;
+    f = factorial(n);
+    cout<<"\n" << f;
+}
+
